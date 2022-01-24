52779c3cbbc9c6581aafaeeb906ffbb6fb59cf6d
wlsgm <wlsgm@DESKTOP-TE32HKT>
2022-01-24 18:53:21 +0900

    220124 구구단 작성

---
+++ Git_Project_1/src/ppractice_1/For_practice.java
@@ -0,0 +1,15 @@
+package ppractice_1;
+
+public class For_practice {
+
+	public static void main(String[] args) {
+		for(int i=2; i<=9; i++) {
+			for(int j=1; j<=9; j++) {
+				System.out.println(i + " * " + j + " = " + i*j);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
